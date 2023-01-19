c13c092497633d872628c035ed562597b9b4dd75
doublebyte1 <info@doublebyte.net>
2023-01-19 16:28:17 +0000

    - fixed submodules

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,5 +3,11 @@ const upath = require('upath');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../dist');
 
-sh.rm('-rf', `${destPath}/*`)
+// sh.rm('-rf', `${destPath}/*`)
 
+// Note: this only removes these paths - use with caution!
+sh.rm('-rf', `${destPath}/assets`)
+sh.rm('-rf', `${destPath}/css`)
+sh.rm('-rf', `${destPath}/js`)
+sh.rm('-rf', `${destPath}/blogs.html`)
+sh.rm('-rf', `${destPath}/index.html`)
