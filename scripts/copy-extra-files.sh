1303a7e013345a8dbaa6e2029552bda822783683
doublebyte1 <info@doublebyte.net>
2024-04-04 18:33:18 +0100

    - updated run scripts to copy extra files to dist folder

--- scripts/copy-extra-files.sh
+++ scripts/copy-extra-files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Use this, after generating the static HTML with "npm run build"
-dir="../dist2"
+# dir="../dist2"
 
-rm -rf "$dir"
-mkdir -p "$dir" || exit 1
-cp ../dist/sponsoring.html "$dir"
-cp -r ../dist/assets "$dir"
-cp -r ../dist/css "$dir"
+# rm -rf "$dir"
+# mkdir -p "$dir" || exit 1
+# cp ../dist/sponsoring.html "$dir"
+# cp -r ../dist/assets "$dir"
+# cp -r ../dist/css "$dir"
+
+cp -r ../dist/sprints.html ../dist/sprints/index.html
+cp -r ../README-dist.md ../dist/README.md
+cp -r ../CNAME ../dist/CNAME
+
 echo "Files copied to $dir"
--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -8,4 +8,16 @@ module.exports = function renderAssets() {
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
     
     sh.cp('-R', sourcePath, destPath)
+
+    const cname = upath.resolve(upath.dirname(__filename), '../CNAME');
+    sh.cp('-R', cname, destPath)
+
+    const readmeIn = upath.resolve(upath.dirname(__filename), '../README-dist.md');
+    const readmeOut = upath.resolve(upath.dirname(__filename), '../dist/README.md');
+    sh.cp('-R', readmeIn, readmeOut)
+
+    const sprintsIn = upath.resolve(upath.dirname(__filename), '../dist/sprints.html');
+    const sprintsOut = upath.resolve(upath.dirname(__filename), '../dist/sprints/index.html');
+    sh.cp('-R', sprintsIn, sprintsOut)
+
 };
