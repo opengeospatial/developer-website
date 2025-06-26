9f9438679ff58d8c5da8b5595332bd84a05a28a3
doublebyte1 <info@doublebyte.net>
2025-06-26 16:57:56 +0100

    - added testemonial from joao

--- scripts/copy-extra-files.sh
+++ scripts/copy-extra-files.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Use this, after generating the static HTML with "npm run build"
-# dir="../dist2"
-
-# rm -rf "$dir"
-# mkdir -p "$dir" || exit 1
-# cp ../dist/sponsoring.html "$dir"
-# cp -r ../dist/assets "$dir"
-# cp -r ../dist/css "$dir"
-
 cp -r ../dist/sprints.html ../dist/sprints/index.html
-cp -r ../README-dist.md ../dist/README.md
+# cp -r ../README-dist.md ../dist/README.md
 cp -r ../CNAME ../dist/CNAME
 
 echo "Files copied to $dir"
